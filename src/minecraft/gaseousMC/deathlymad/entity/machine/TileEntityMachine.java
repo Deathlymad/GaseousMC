75e49d2ca27f6c19a86eb1acfa5127076a15545b
Deathlymad <meyerphilip96@googlemail.com>
2014-06-16 12:26:12 -0700

    added BC and IC2 interfaces
    I have to implement the interfaces in the project

--- src/minecraft/gaseousMC/deathlymad/entity/machine/TileEntityMachine.java
+++ src/minecraft/gaseousMC/deathlymad/entity/machine/TileEntityMachine.java
@@ -1,14 +1,23 @@
 package gaseousMC.deathlymad.entity.machine;
 
 import gaseousMC.deathlymad.Enum.gas;
+
 import net.minecraft.tileentity.TileEntity;
 
-public class TileEntityMachine extends TileEntity {
+import ic2.api.energy.tile.IEnergySink;
+
+import buildcraft.api.power.IPowerReceptor;
+import buildcraft.api.power.PowerHandler;
+
+public class TileEntityMachine extends TileEntity implements IEnergySink /*IC2 Energy Interface */, IPowerReceptor /*BC Power Interface*/ {
 
 	public gas[] spawnsSmoke;
 	int tickcounter=0;
+	int TickDelay=20
 	
-	public TileEntityMachine(gas[] spawnsSmoke2) {
+	public final PowerHandler BCPowerHandler = new PowerHangdler(this, MACHINE);
+	
+	public TileEntityMachine(gas[] spawnsSmoke2 /*Sets the smoke being emmitted while machine is running*/) {
 		spawnsSmoke=spawnsSmoke2;
 	}
 
@@ -17,14 +26,14 @@ public class TileEntityMachine extends TileEntity {
 	{
 		tickcounter++;
 		
-		if(tickcounter>=100)
+		if(tickcounter>=TickDelay)
 		{
 			if (spawnsSmoke[0]!=gas.ERRGAS)
-				{
-					if(this.worldObj.getBlockId(this.xCoord, this.yCoord+1, this.zCoord)==0)
-						this.worldObj.setBlock(this.xCoord, this.yCoord+1, this.zCoord ,0);
+			{
+				if(this.worldObj.getBlockId(this.xCoord, this.yCoord+1, this.zCoord)==0)
+					this.worldObj.setBlock(this.xCoord, this.yCoord+1, this.zCoord ,0);
 					
-				}
+			}
 			tickcounter=0;
 		}
 	}
